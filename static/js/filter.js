8433ff464138b7c511ce1aa3778f23726e679236
Adil Shabab <adilshaba@gmail.com>
2022-08-08 04:27:13 -0700

    filter section completed

--- static/js/filter.js
+++ static/js/filter.js
@@ -2,9 +2,9 @@ var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 30,
     grabCursor: true,
-    // autoplay: {
-    //     delay: 4000,
-    //     disableOnInteraction: false,
-    //   },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
     loop: true,
   });
