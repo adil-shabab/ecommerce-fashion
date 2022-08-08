7107ae80147556747fd3b96841c857c2a49cd315
Adil Shabab <adilshaba@gmail.com>
2022-08-08 03:18:45 -0700

    filter section progressing

---
+++ static/js/filter.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    grabCursor: true,
+    // autoplay: {
+    //     delay: 4000,
+    //     disableOnInteraction: false,
+    //   },
+    loop: true,
+  });
