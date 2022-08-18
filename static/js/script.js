0e05437ed2711b1ec56e78551610bcd81fe3f17d
Adil Shabab <adilshaba@gmail.com>
2022-08-17 23:22:43 -0700

    single page left section completed

--- static/js/script.js
+++ static/js/script.js
@@ -10,9 +10,41 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-//   console.log('working')
+
+  // single page js 
+var mainImg = document.getElementById('main-img')
+var smallImg = document.getElementsByClassName('small-image')
+console.log(smallImg[1])
 
 
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src
+    smallImg[0].classList.add('active')
+    smallImg[1].classList.remove('active')
+    smallImg[2].classList.remove('active')
+    smallImg[3].classList.remove('active')
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src
+    smallImg[1].classList.add('active')
+    smallImg[0].classList.remove('active')
+    smallImg[2].classList.remove('active')
+    smallImg[3].classList.remove('active')
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src
+    smallImg[2].classList.add('active')
+    smallImg[0].classList.remove('active')
+    smallImg[1].classList.remove('active')
+    smallImg[3].classList.remove('active')
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src
+    smallImg[3].classList.add('active')
+    smallImg[0].classList.remove('active')
+    smallImg[1].classList.remove('active')
+    smallImg[2].classList.remove('active')
+}
 
 
 
